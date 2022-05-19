c1e48a02344e2d131866157ab58c9fc9d867b515
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2022-05-19 11:55:54 +0300

    Create IGenericRepository.cs

---
+++ my codes/repository pattern/IGenericRepository.cs
@@ -0,0 +1,6 @@
+public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        IEnumerable<T> FindWithSpecificationPattern(ISpecification<T> specification = null);
+    }
